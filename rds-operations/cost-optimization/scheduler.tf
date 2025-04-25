0067c0656ad678b0680cf8d07f15666fb81caef4
nano_dorado <nano_dorado@hotmail.com>
2025-04-25 11:32:27 -0300

    Update scheduler.tf

--- rds-operations/cost-optimization/scheduler.tf
+++ rds-operations/cost-optimization/scheduler.tf
@@ -1,18 +1,18 @@
-﻿resource \"aws_cloudwatch_event_rule\" \"stop_rds_schedule\" {
-  name                = \"stop-rds-dev-daily\"
-  schedule_expression = \"cron(0 23 * * ? *)\" # 8pm Buenos Aires
+﻿resource "aws_cloudwatch_event_rule" "stop_rds_schedule" {
+  name                = "stop-rds-dev-daily"
+  schedule_expression = "cron(0 23 * * ? *)" # 8pm Buenos Aires
 }
 
-resource \"aws_cloudwatch_event_target\" \"stop_rds_target\" {
+resource "aws_cloudwatch_event_target" "stop_rds_target" {
   rule      = aws_cloudwatch_event_rule.stop_rds_schedule.name
-  target_id = \"StopRDSInstance\"
+  target_id = "StopRDSInstance"
   arn       = aws_lambda_function.stop_rds.arn
 }
 
-resource \"aws_lambda_permission\" \"allow_events\" {
-  statement_id  = \"AllowExecutionFromCloudWatch\"
-  action        = \"lambda:InvokeFunction\"
+resource "aws_lambda_permission" "allow_events" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.stop_rds.function_name
-  principal     = \"events.amazonaws.com\"
+  principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.stop_rds_schedule.arn
 }
